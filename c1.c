5ef8869b88f4b41e701596a550a88af88e6a9ff9
stfe30 <101103094+stfe30@users.noreply.github.com>
2022-04-21 13:41:23 +0200

    Update c1.c

--- c1.c
+++ c1.c
@@ -1,5 +1,5 @@
 // TEST-File: Division durch Null
-
+*/
 #include <stdio.h>
 int main() {
     int dividend, divisor, quotient, remainder;
@@ -17,4 +17,26 @@ int main() {
     printf("Quotient = %d\n", quotient);
     printf("Remainder = %d", remainder);
     return 0;
+}
+/*
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[]) {
+    int score = 1;
+
+START:
+    if (score > 1000)
+        goto EXIT;
+
+    score += 1;
+    goto START;
+
+EXIT:
+    printf("score: %d\n", score);
+    exit(EXIT_SUCCESS);
 }
